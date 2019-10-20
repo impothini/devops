df605816e96df2b9521c5ea902d5d23211044fee
Syam Pothini <impothini@gmail.com>
2019-10-20 07:53:23 +0000

    using consul module

---
+++ terraform/modules/vars.tf
@@ -0,0 +1,19 @@
+variable "AWS_REGION"{
+	default="us-east-2"
+}
+variable "AMIS" {
+	type="map"
+	default= {
+	"us-east-1" = "ami-04763b3055de4860b"
+	"us-west-1" = "ami-0dbf5ea29a7fc7e05"
+	"us-east-2" = "ami-0d03add87774b12c5"
+	}
+}
+
+variable "PATH_TO_PRIVATE_KEY" {
+	default="mykey"
+}
+
+variable "PATH_TO_PUBLIC_KEY" {
+	default="mykey.pub"
+}
