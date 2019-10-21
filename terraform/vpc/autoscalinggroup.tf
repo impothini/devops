339ff98994c3143e79e72906d3887b58d7dbef18
Syam Pothini <impothini@gmail.com>
2019-10-21 20:25:22 +0000

    create autoscaling group

---
+++ terraform/vpc/autoscalinggroup.tf
@@ -0,0 +1,14 @@
+resource "aws_autoscaling_group" "auto-scaling-group-1" {
+  desired_capacity          = 1
+  max_size                  = 2
+  min_size                  = 1
+  health_check_type         = "EC2"
+  health_check_grace_period = 300
+  vpc_zone_identifier       = ["${aws_subnet.public-subnet-3.id}", "${aws_subnet.public-subnet-2.id}"]
+
+  tags {
+    key                 = "Name"
+    value               = "ec2 Instance"
+    propagate_at_launch = true
+  }
+}
